4d8e4f53c43452271cca480e8bb590d2250a943a
Raihan92 <raihancse1992@gmail.com>
2020-09-03 23:01:08 +0600

    rest service tutorial

---
+++ src/main/java/com/admin/webservice/mavenrest/MavenRestApplication.java
@@ -0,0 +1,13 @@
+package com.admin.webservice.mavenrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin/webservice/mavenrest/MavenRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.webservice.mavenrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
